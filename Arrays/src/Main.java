d8394d233b146166d9bf1552db871f6c0060b414
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-01-30 09:59:31 +0000

    Intial Commit

---
+++ Arrays/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers  = new int [3]; // declaring an array with size 3
+        System.out.println(numbers);
+
+    }
+}
---
+++ Big O/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public void long {int [] numbers}
+        for (int first : numbers);
+            for (int second: numbers);
+        System.out.println(first " " + second);
+    }
+}
