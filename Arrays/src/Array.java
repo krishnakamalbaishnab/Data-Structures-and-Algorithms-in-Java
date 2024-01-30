05836f4fa3d38c311d96962c42d78d689dccfe61
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-01-30 18:33:06 +0000

    Creating an array class, inserting an item, deleting an item

---
+++ Arrays/src/Array.java
@@ -0,0 +1,45 @@
+public class Array {
+
+    // Private members to store array elements and count of elements
+    private int[] items;
+    private int count; //the count variable is intended to keep track of the number of elements currently
+    // present in the array. However, in the current implementation,
+    // the count variable is not being updated anywhere in the code, so its value will always be zero.
+
+    // Constructor to initialize the array with a given length
+    public Array(int length) {
+        items = new int[length];
+    }
+    //inserting items in array
+    public void insert (int item){
+        if (items.length == count){
+            int[] newItems = new int[count * 2]; // declaring an new array so that it becomes dynamic and the size is twice the size of the items array
+            //copying all the existing items
+            for (int i = 0; i <count; i++)
+                newItems[i]= items[i];
+            //setting "items" to this new array
+            items = newItems;
+        }
+        items[count++]= item;
+    }
+
+    //deleting an item from an array
+    public void removeAt(int index){
+        //validating the index
+        if(index<0|| index>=count)
+            throw new IllegalArgumentException();
+
+        //shift the items to the left to fill the hole
+
+        for(int i = index; i<count; i++)
+            items[i] = items[i+1];
+
+    count --;
+    }
+    // Method to print the elements of the array
+    public void print() {
+        // Iterate through the array and print each element
+        for (int i = 0; i < count; i++)
+            System.out.println(items[i]);
+    }
+}
--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -2,20 +2,30 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers  = new int [3]; // declaring an array with size 3
-        numbers[0] = 10;
-        numbers[1] = 20;
-        numbers[2] = 30;
-        //this lines of code is storing items to the array named "numbers" with the index value
-        System.out.println(Arrays.toString(numbers)); // Arrays is method in java from the package java.util
-        //what is line of code is doing is that , Arrays toString is converting the object to string and then printing o the console. It will print 0 , 0, 0
-        //now if we want to print or store different value to the arrays how we can do is that *GO TO LINE NUMBER 6*
-        // shorthand of declaring and initialising an array
+//        int[] numbers  = new int [3]; // declaring an array with size 3
+//        numbers[0] = 10;
+//        numbers[1] = 20;
+//        numbers[2] = 30;
+//        //this lines of code is storing items to the array named "numbers" with the index value
+//        System.out.println(Arrays.toString(numbers)); // Arrays is method in java from the package java.util
+//        //what is line of code is doing is that , Arrays toString is converting the object to string and then printing o the console. It will print 0 , 0, 0
+//        //now if we want to print or store different value to the arrays how we can do is that *GO TO LINE NUMBER 6*
+//        // shorthand of declaring and initialising an array
+//
+//        int[]  numbers2 = {40,50,60}; //short syntax of declaring an array in Java
+//        System.out.println(Arrays.toString(numbers2));
+//
+//        //printing the size of the array
+//        System.out.println(numbers2.length);
 
-        int[]  numbers2 = {40,50,60}; //short syntax of declaring an array in Java
-        System.out.println(Arrays.toString(numbers2));
+        Array number = new Array(3);
+        number.insert(100);
+        number.insert(200);
+        number.insert(300);
+        number.insert(400);
 
-        //printing the size of the array
-        System.out.println(numbers2.length);
+        number.removeAt(3);
+        number.print();
+
     }
 }
